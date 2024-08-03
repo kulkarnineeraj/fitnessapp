7232225a977fa6e3f4c77c844027738a76b02a88
kulkarnineeraj <neerajkulk@gmail.com>
2024-08-03 17:21:34 +0530

    Deployment

--- src/test/java/com/example/fitness/FitnessApplicationTests.java
+++ src/test/java/com/example/fitness/FitnessApplicationTests.java
@@ -1,9 +1,11 @@
-package com.example.Fitness.fitness;
+package com.example.fitness;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootApplication
 class FitnessApplicationTests {
 
 	@Test
