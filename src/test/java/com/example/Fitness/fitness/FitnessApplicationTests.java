8617448be774456da835943098da565d59888c9e
kulkarnineeraj <neerajkulk@gmail.com>
2024-08-04 14:41:20 +0530

    @ContextConfiguration added

--- src/test/java/com/example/Fitness/fitness/FitnessApplicationTests.java
+++ src/test/java/com/example/Fitness/fitness/FitnessApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.Fitness.fitness;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration
 @SpringBootTest
 class FitnessApplicationTests {
 
