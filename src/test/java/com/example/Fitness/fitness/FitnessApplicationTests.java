ae9e44eb33d191f364e8fdb22da69a76852ae94c
kulkarnineeraj <neerajkulk@gmail.com>
2024-08-04 14:44:36 +0530

    trest

--- src/test/java/com/example/Fitness/fitness/FitnessApplicationTests.java
+++ src/test/java/com/example/Fitness/fitness/FitnessApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration
+@ContextConfiguration(classes=FitnessApplicationTests.class)
 @SpringBootTest
 class FitnessApplicationTests {
 
